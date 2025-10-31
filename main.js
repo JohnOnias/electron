2f6a9471b30ec4eded66716669cc6cec860a4cdb
John E. Onias <119425650+JohnOnias@users.noreply.github.com>
2025-10-31 11:03:23 -0300

    login feito, falta a mudança de senha

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
-const { app, BrowserWindow, nativeTheme } = require('electron')
-const sqlite3 = require('sqlite3').verbose(); 
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const {session } = require('electron');
 
 
-//conecção com o db
-const db = new sqlite3.Database(path.join(__dirname, './src/public/BD/database.db'), (err) =>{
-if (err){
-  console.error('Database connection failedd:', err); 
-} 
-else {
-  console.log('Database connected!'); 
+
+// conexão com o banco de dados
+function conn() {
+    return new Promise((resolve, reject) => {
+        const db = new sqlite3.Database(
+            path.join(__dirname, './src/public/BD/database.db'), 
+            (err) => {
+                if (err) {
+                    console.error('Database connection failed:', err); 
+                    reject(err);
+                } else {
+                    console.log('Database connected!'); 
+                    resolve(db);
+                }
+            }
+        );
+    });
 }
-});
+// função de login e auth
+async function loginAuth(email, senha) {
+    const db = await conn();
+
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT 'adm' as tipo, id, nome, email 
+            FROM tb_Administrador 
+            WHERE email = ? AND senha = ?
+            
+            UNION ALL  
+            
+            SELECT 'funcionario' as tipo, id, nome, email 
+            FROM tb_Funcionarios 
+            WHERE email = ? AND senha = ?
+        `;
+        
+        db.all(query, [email, senha, email, senha], (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+            db.close();
+            
+        });
+    });
+}
 
 
-// cria a janela index
+
+// finção pra criar as telas cria as telas(pfv não toque nisso)
 const createWindow = () => {
     nativeTheme.themeSource = 'dark'
   const win = new BrowserWindow({
@@ -22,6 +61,7 @@ const createWindow = () => {
     height: 600,
     resizable: false,
     autoHideMenuBar: true
+
     //para adcionar um icone
     //,
     //icon: 'local da imagem aqui'
@@ -30,31 +70,37 @@ const createWindow = () => {
   win.loadFile('./src/views/index.html')
 }
 
-const sobreWindow = () => {
-    nativeTheme.themeSource = 'dark'
-    const sobre = new BrowserWindow({
-        width: 360,
-        height: 220,
-        resizable: false,
-        autoHideMenuBar: true
-    })
-    // renderiza a janela 
-    sobre.loadFile('./src/views/sobre.html')
+// função pra criar a tela de login 
+const loginWindow = () => {
+    nativeTheme.themeSource = 'dark';
+    const login = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        resizable: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true, 
+            nodeIntegration: false
+        }
+    });
+    login.loadFile('./src/views/login.html');
 }
-
 
 // aqui chama a janela principal quando se clica no app
 app.whenReady().then(() => {
-  createWindow()
-  sobreWindow()
+  loginWindow(); 
 
-  // so abre outra janela se todas estiverem fechadas (para MAC)
+// so abre outra janela se todas estiverem fechadas (para MAC)
  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+});
 
 // quando a janela for fechada encerra a aplicação (para WIN e Linux)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
-})
+});
+// rota de login
+ipcMain.handle("login-auth", async (event, email, senha) => {
+   return await loginAuth(email, senha);
+});
---
+++ src/views/rendererLogin.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector('#entrar');
+
+btn.addEventListener("click", async () => {
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    try {
+        const usuario = await window.api.login(email, senha);
+
+        console.log('Resposta do main:', usuario);
+        //alert('Resposta completa:\n' + JSON.stringify(usuario, null, 2));
+
+        if (usuario.length > 0) {
+            alert('Login bem-sucedido: ' + JSON.stringify(usuario[0]));
+        } else {
+            alert('Email ou senha incorretos');
+        }
+
+    } catch (error) {
+        console.error('Erro ao chamar login:', error);
+        alert('Erro no login: ' + error);
+    }
+});
+
